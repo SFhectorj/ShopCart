db43baacb2a79069189059f08542d554c8c7f491
SFhectorj <baezahectorj@gmail.com>
2025-11-01 14:34:54 -0700

    Implementing Main Menu

---
+++ main.py
@@ -0,0 +1,23 @@
+# Handle user interaction and navigation between menus
+
+def main_menu():
+    """
+    Display main menu and prompt user for input
+    """
+    while True:
+        print("\n============================")
+        print("         SHOPCART")
+        print("For all of you daily needs!")
+        print("============================")
+        print("1. Browse Products")
+        print("2. View Cart")
+        print("3. Checkout")
+        #Print("Accessibility")
+        print("5. Exit")
+
+        user_choice = input("You've Got Options!:")
+        break
+    else:
+        print("Invalid Option")
+
+main_menu()
